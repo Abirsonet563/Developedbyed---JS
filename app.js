394ab1c04ed2df8c16497c0385e1ef8e6efb017e
Abir Hasan <abir.hasan@pickles.com.au>
2024-09-27 20:39:28 +0100

    var/let/const assign

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ const age = 30;
 var balance = 230;
 var balance = 1200; // var will allow me to change that.
 console.log(life, balance);
+
+let debt = 1000;
+debt = debt - 500; // let will allow me to change the number
+console.log(debt);
