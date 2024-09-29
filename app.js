ab9970d52a322742cfef7d85aaea5f9cadf436f3
Abir Hasan <abir.hasan@pickles.com.au>
2024-09-29 12:40:15 +0100

    For and While Loop

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const user = {
-    name: "Abir Hasan",
-    age: "23",
-    married: "False",
-    purchase: ["Mobile","Laptop","Bag"],
-    sayName: function(){
-        console.log(this.name);
+//For Loop
+
+const name = ["Abir","Hasan","Saif","Hasan"]
+
+for (names of name){
+    console.log(names);
+    console.log(`Hello There ${names}`)
+}
+
+//Add condition inside Foor Loop
+for (names of name){
+    if (names === "Abir"){
+        console.log("Abir In my List");
+        // break;
     }
-};
- user.sayName();
-// console.log(user.sayName,user.name);
-
-function apple (){
-    console.log("apples")
 }
-//console.log(this);
 
-//Focuse default window object 
-function nameTwo(){
-    console.log(this)
+//while loop
+let loading = 100
+
+while(loading < 1000){
+    console.log("Still Loading")
+    loading ++;
 }
-nameTwo();
+
+while (loading < 1001){
+    console.log("Still Loading")
+    loading += 1;
+}
